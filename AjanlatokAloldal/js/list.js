b4c3333523ea45014c4361b614ac304083d84812
LakatosBotond <lakatosbotond@turr.hu>
2026-03-09 09:39:15 +0100

    Mappa fix

--- AjanlatokAloldal/js/list.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener("click", (e) => {
-  const btn = e.target.closest(".megtekitentesGomb");
-  if (!btn) return;
-
-  const id = btn.dataset.id;
-  window.location.href = `ajanlatok_minta.html?id=${encodeURIComponent(id)}`;
-});
