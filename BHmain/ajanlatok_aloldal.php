37f0c428db7802c0aa3a91ddb780226e4035cee7
BosnyakBorisz <bosnyakborisz@turr.hu>
2026-04-16 12:28:19 +0200

    css javitas

--- BHmain/ajanlatok_aloldal.php
+++ BHmain/ajanlatok_aloldal.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Baja látnivalók</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
-    <link rel="stylesheet" href="ajanlatok.css">
+    <link rel="stylesheet" href="main.css">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.css">
 </head>
 <body id="latnivalok_hatter">
