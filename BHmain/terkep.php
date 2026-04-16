8cef43b40e7440cec20a5f6e80c6f9e64b701fa4
BosnyakBorisz <bosnyakborisz@turr.hu>
2026-04-16 12:15:52 +0200

    css

--- BHmain/terkep.php
+++ BHmain/terkep.php
@@ -12,23 +12,14 @@
     <header>
     <?php  include("header.php");?>
     </header>
+    <main style="width: 100%; height: 80vh;">
+        <iframe src="https://www.google.com/maps?q=6500+Baja,+B%C3%A1cska+t%C3%A9r+1.&output=embed" 
+                        width="100%" height="100%" style="border:0;" allowfullscreen="" loading="lazy" ></iframe>
+    </main>
+    <?php  include("footer.html");?>
 
-
-
-
-
-<main >
-    <iframe src="https://www.google.com/maps?q=6500+Baja,+B%C3%A1cska+t%C3%A9r+1.&output=embed" 
-                      width="100%" height="100%" style="border:0;" allowfullscreen="" loading="lazy" ></iframe>
-</main>
-
-
-
-<?php  include("footer.html");?>
-
-
-  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
-  <script src="main.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
+    <script src="main.js"></script>
 
 </body>
 </html>
